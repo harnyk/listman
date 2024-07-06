c11091cf7c92fe0b059e5400e39ddc2aa66ac505
Mark <markh@sweepbright.com>
2024-07-06 11:41:44 +0200

    fix: do not redirect to the deleted tree

--- src/hooks/useTreeIdByImportedListId.ts
+++ src/hooks/useTreeIdByImportedListId.ts
@@ -3,7 +3,7 @@ import { useDependencies } from './useDependencies';
 import { swrKeyTreeIdByImportedListId } from './swr/keys';
 
 export function useTreeIdByImportedListId(importedListId: string) {
-    const { checkoutMetadataRepo } = useDependencies();
+    const { checkoutMetadataRepo, treeRepo } = useDependencies();
     return useSWR(
         swrKeyTreeIdByImportedListId(importedListId),
         async () => {
@@ -11,6 +11,16 @@ export function useTreeIdByImportedListId(importedListId: string) {
                 await checkoutMetadataRepo.getTreeIdByImportedListId(
                     importedListId
                 );
+
+            if (!treeId) {
+                return null;
+            }
+
+            const treeItem = await treeRepo.getItem(treeId, false);
+            if (!treeItem) {
+                return null;
+            }
+
             return treeId;
         },
         { revalidateOnFocus: false }
