fe9b4a4f83264cf03cb53af331319ab42173ed3e
Mark <markh@sweepbright.com>
2024-07-23 11:42:23 +0200

    fix: add audio mimetypes

---
+++ pkg/botservice/mimetypes.go
@@ -0,0 +1,40 @@
+package botservice
+
+import (
+	"mime"
+)
+
+var mimetypes = [][]string{
+	{"audio/amr", "amr"},
+	{"audio/amr-wb", "awb"},
+	{"audio/annodex", "axa"},
+	{"audio/basic", "au", "snd"},
+	{"audio/csound", "csd", "orc", "sco"},
+	{"audio/flac", "flac"},
+	{"audio/midi", "mid", "midi", "kar"},
+	{"audio/mpeg", "mpga", "mpega", "mp2", "mp3", "m4a"},
+	{"audio/mpegurl", "m3u"},
+	{"audio/ogg", "oga", "ogg", "opus", "spx"},
+	{"audio/prs.sid", "sid"},
+	{"audio/x-aiff", "aif", "aiff", "aifc"},
+	{"audio/x-gsm", "gsm"},
+	{"audio/x-mpegurl", "m3u"},
+	{"audio/x-ms-wma", "wma"},
+	{"audio/x-ms-wax", "wax"},
+	{"audio/x-pn-realaudio", "ra", "rm", "ram"},
+	{"audio/x-realaudio", "ra"},
+	{"audio/x-scpls", "pls"},
+	{"audio/x-sd2", "sd2"},
+	{"audio/x-wav", "wav"},
+}
+
+func init() {
+	for _, v := range mimetypes {
+		typ := v[0]
+		extensions := v[1:]
+
+		for _, ext := range extensions {
+			mime.AddExtensionType(ext, typ)
+		}
+	}
+}
